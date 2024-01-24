6c674b233e847d88f1667b663c9a4f04d3553f09
zubairasif3 <119746286+zubairasif3@users.noreply.github.com>
2024-01-24 22:26:08 +0500

    ch

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,58 +10,69 @@ jQuery("#hero-carousel").owlCarousel({
     items: 1
 });
 jQuery("#testimonial-carousel").owlCarousel({
-    autoplay: false,
-    rewind: false,
-    loop: true,
-    margin: 20, 
-    responsiveClass: true,
-    autoHeight: true,
-    nav: true,
-    dots: false,
-    responsive: {
-      0: {
-        items: 1
-      },
-  
-      600: {
-        items: 2
-      },
-  
-      1024: {
-        items: 3
-      },
-  
-      1366: {
-        items: 3
-      }
+  autoplay: false,
+  rewind: false,
+  loop: true,
+  margin: 20, 
+  responsiveClass: true,
+  autoHeight: true,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2
+    },
+
+    1024: {
+      items: 3
+    },
+
+    1366: {
+      items: 3
     }
+  }
 });
 jQuery("#video-carousel").owlCarousel({
-    autoplay: false,
-    rewind: false,
-    loop: true,
-    margin: 20, 
-    responsiveClass: true,
-    autoHeight: true,
-    nav: true,
-    dots: false,
-    responsive: {
-      0: {
-        items: 1
-      },
-  
-      600: {
-        items: 3
-      },
-  
-      1024: {
-        items: 4
-      },
-  
-      1366: {
-        items: 4
-      }
+  autoplay: false,
+  rewind: false,
+  loop: true,
+  margin: 20, 
+  responsiveClass: true,
+  autoHeight: true,
+  nav: true,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 3
+    },
+
+    1024: {
+      items: 4
+    },
+
+    1366: {
+      items: 4
     }
+  }
+});
+jQuery("#projects-carousel").owlCarousel({
+  autoplay: false,
+  rewind: false,
+  loop: true,
+  margin: 20, 
+  responsiveClass: true,
+  autoHeight: true,
+  nav: false,
+  dots: false,
+  items: 2.5
 });
 $( ".owl-prev").html('<i class="fa fa-long-arrow-alt-left"></i>');
 $( ".owl-next").html('<i class="fa fa-long-arrow-alt-right"></i>');
